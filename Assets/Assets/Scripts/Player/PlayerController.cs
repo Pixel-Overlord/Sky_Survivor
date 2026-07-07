22274e45a619f790c198f224dee5e20db8335f02
Satish Jha <jha.satish0210@gmail.com>
2026-07-07 18:08:56 +0530

    Added player movement

--- Assets/Assets/Scripts/Player/PlayerController.cs
+++ Assets/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,52 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Script Name: PlayerController
+ * 
+ * What it does: 
+ * - This script is responsible for controlling the player(ship) in the game.
+ * - It handles player input, movement, and interactions with the game world.
+ */
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 20f;
+    [SerializeField] private float pitchSpeed = 60f;
+    [SerializeField] private float maxPitchAngle = 30f;
+
+    private float currentPitch;
+    private float targetPitch;
+
+    private void Update()
     {
-        
+        handlePitch();
+        moveForward();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void handlePitch()
     {
-        
+        float input = Input.GetAxis("Vertical");
+
+        targetPitch = input * maxPitchAngle;
+
+        if (input != 0)
+        {
+            transform.Rotate(input * pitchSpeed * Time.deltaTime, 0f, 0f);
+        }
+        else
+        {
+            currentPitch = Mathf.Lerp(
+                currentPitch,
+                targetPitch,
+                pitchSpeed * Time.deltaTime);
+
+            transform.localRotation = Quaternion.Euler(currentPitch, 0f, 0f);
+        }
+    }
+
+    private void moveForward()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
     }
 }
